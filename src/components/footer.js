b014e3f2287b94c1885b97cc02b21758b9aa0e2f
Ricardo Marliere <rbmarliere@gmail.com>
2018-12-16 11:22:18 +0000

    make Footer and Main stateless

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,26 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-class Footer extends Component
+const Footer = () =>
 {
-    render()
-    {
-        return (
-            <footer className="container-fluid" expand="md">
-                <div className="row d-flex justify-content-center pt-2 pb-2">
-                    <div className="col-xs-12">
-                        <p className="lightgray text-justify">
-                            darma {( new Date().getFullYear() )} &copy; ricardo@eossweden.org &nbsp;
-                            <a href="https://github.com/rbmarliere/darma">
-                                <FontAwesomeIcon icon={ ["fab", "github"] } size="lg"/>
-                            </a>
-                        </p>
-                    </div>
+    return (
+        <footer className="container-fluid" expand="md">
+            <div className="row d-flex justify-content-center pt-2 pb-2">
+                <div className="col-xs-12">
+                    <p className="lightgray text-justify">
+                        darma {( new Date().getFullYear() )} &copy; ricardo@eossweden.org &nbsp;
+                        <a href="https://github.com/rbmarliere/darma">
+                            <FontAwesomeIcon icon={ ["fab", "github"] } size="lg"/>
+                        </a>
+                    </p>
                 </div>
-            </footer>
-        );
-    }
-}
+            </div>
+        </footer>
+    );
+};
 
 export default Footer;
 
--- src/components/main.js
+++ src/components/main.js
@@ -1,30 +1,18 @@
-import React, { Component } from "react";
-
+import React from "react";
 import NavBar from "./navbar";
 import ProjectCard from "./projectCard";
 import Footer from "./footer";
 
-class Main extends Component
+const Main = () =>
 {
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            userLogged: false,
-        };
-    }
-
-    render()
-    {
-        return (
-            <React.Fragment>
-                <NavBar></NavBar>
-                <ProjectCard></ProjectCard>
-                <Footer></Footer>
-            </React.Fragment>
-        );
-    }
-}
+    return (
+        <React.Fragment>
+            <NavBar></NavBar>
+            <ProjectCard></ProjectCard>
+            <Footer></Footer>
+        </React.Fragment>
+    );
+};
 
 export default Main;
 
