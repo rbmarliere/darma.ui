09dc55ac4034252c18e2b162d2649e1ec449f70e
Ricardo Marliere <rbmarliere@gmail.com>
2019-01-16 16:49:03 +0000

    fetch project list from chain in projectList.js

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -1,22 +1,41 @@
 import React, { Component } from "react";
 import Project from "../containers/project";
-import apps from "../helpers/apps";
 import {
     Col,
     Container,
     Row
 } from "reactstrap";
+import { rpc } from "../helpers/scatter";
 
 class ProjectList extends Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.state = { listings: [] };
+    }
+
+    componentDidMount()
+    {
+        rpc.get_table_rows(
+            {
+                code: "stakemine112",
+                scope: "stakemine112",
+                table: "listing"
+            }
+        ).then(
+            res => this.setState({ listings: res.rows })
+        );
+    }
+
     render()
     {
         return (
             <Container className="main">
                 <Row>
                     {
-                        apps.map( (data, i) => (
-                            <Col key={i} className="project" lg="4" sm="6" xs="12">
+                        this.state.listings.map( (data, i) => (
+                            <Col key={i} className="project" lg="4">
                                 <Project {...data} />
                             </Col>
                         ))
